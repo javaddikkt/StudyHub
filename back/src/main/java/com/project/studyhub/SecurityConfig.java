07d0ca88ba2ef3b925dff4cb555986bbabe824bd
thektrnesis <408775@niuitmo.ru>
2025-04-10 00:07:17 +0300

    cors config changed

--- back/src/main/java/com/project/studyhub/SecurityConfig.java
+++ back/src/main/java/com/project/studyhub/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
                                 .requestMatchers("/api/auth/**").permitAll()
