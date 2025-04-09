48f8831847d4bd6b770e5de4466c763f6fcba76b
thektrnesis <408775@niuitmo.ru>
2025-04-10 00:03:12 +0300

    allowed credentials

--- back/src/main/java/com/project/studyhub/SecurityConfig.java
+++ back/src/main/java/com/project/studyhub/SecurityConfig.java
@@ -80,7 +80,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("https://study-hub-kohl.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
